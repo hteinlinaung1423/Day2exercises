d5e3223ec9ae51714f81de23884aa4c4fe98820d
hteinlinaung <lovebreakerboy.lin@gmail.com>
2017-02-06 16:00:05 +0800

    Add project files.

---
+++ Day2exercises/exe1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2exercises
+{
+    class exe1
+    {
+        static void Main(string[] args)
+        {
+            double d;
+            Console.Write("Enter a double precision number : ");
+            d = Convert.ToDouble(Console.ReadLine());
+            d = Math.Sqrt(d);
+            Console.WriteLine("Square root of the number : {0} ", d);
+
+        }
+    }
+}
