506fcffc8afd5cacaf7a803505eb263d59af741c
hteinlinaung <lovebreakerboy.lin@gmail.com>
2017-02-06 16:49:58 +0800

    I remove ur change

--- Day2exercises/exe2.cs
+++ Day2exercises/exe2.cs
@@ -6,18 +6,16 @@ using System.Threading.Tasks;
 
 namespace Day2exercises
 {
-    class exe1
+    class exe2
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double d;
             Console.Write("Enter a double precision number : ");
             d = Convert.ToDouble(Console.ReadLine());
             d = Math.Sqrt(d);
-            Console.WriteLine("Square root of the number : {0} ", d);
-            
-
-
+            Console.WriteLine("The square root of the number rounded to 3 decimal : {0:0.000}", d);
         }
+
     }
 }
---
+++ Day2exercises/exe3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2exercises
+{
+    class exe3
+    {
+        static void Main()
+        {
+            double s, t, h;
+            Console.Write("Enter a salary : ");
+            s = Convert.ToDouble(Console.ReadLine());
+
+
+        }
+    }
+}
